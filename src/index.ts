e7c7b828d0864d515372f170e02135f791b24cc9
Manuel Yang <3444220+mannyyang@users.noreply.github.com>
2025-04-06 23:10:31 -0600

    Update Geolocation API to return the request object for debugging

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,8 @@ interface Env {}
 const app = new Hono<{ Bindings: Env }>();
 
 app.get('/geolocation', (c) => {
-  const geo = c.req.cf;
-
-  if (!geo) {
-    return c.html('<p>Geolocation data is not available.</p>', 500);
-  }
-
-  let html_content = '<h1>Geolocation Data</h1>';
-  html_content += '<p>Colo: ' + geo.colo + '</p>';
-  html_content += '<p>Country: ' + geo.country + '</p>';
-  html_content += '<p>City: ' + geo.city + '</p>';
-  html_content += '<p>Continent: ' + geo.continent + '</p>';
-  html_content += '<p>Latitude: ' + geo.latitude + '</p>';
-  html_content += '<p>Longitude: ' + geo.longitude + '</p>';
-  html_content += '<p>PostalCode: ' + geo.postalCode + '</p>';
-  html_content += '<p>MetroCode: ' + geo.metroCode + '</p>';
-  html_content += '<p>Region: ' + geo.region + '</p>';
-  html_content += '<p>RegionCode: ' + geo.regionCode + '</p>';
-  html_content += '<p>Timezone: ' + geo.timezone + '</p>';
-
-  return c.html(html_content);
+  // Return the entire request object as JSON for debugging purposes
+  return c.json(c.req);
 });
 
 export default app;
